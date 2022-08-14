268e8a69744e0103146f3dc941e88dda71f4edad
Magnus <magnus@magnuss-mbp.lan>
2022-08-14 15:24:13 +0200

    add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+      'index.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'press-start': ['"Bowlby One SC"', 'cursive'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
